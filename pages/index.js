624bfb8c3c938dba7720db47504af8e00b2359a1
Nelson <egwuchinedu69@gmail.com>
2020-06-06 22:22:59 +0100

    change uri port

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Footer from '../src/components/Footer';
 import StateCard from '../src/components/MainLayout/StateCard';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: "http://localhost:8080/graphql"
 })
 
 const LandingPage = () => {
